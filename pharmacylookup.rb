7ec605022bd7b29d54fcea94f5d64dcf6fa25d31
Adam Harbert <Adam.harbert@gmail.com>
2022-11-11 09:58:49 -0500

    New udpates with CSV/pry

--- pharmacylookup.rb
+++ pharmacylookup.rb
@@ -1,9 +1,18 @@
+require 'csv'
 require 'faraday'
 require 'pry'
 response = Faraday.get('https://npiregistry.cms.hhs.gov/api/?organization_name=Onco360&city=&state=&version=2.1')
 #puts response.body
 parsed_json = JSON.parse(response.body)
-parsed_json["results"].each do | pharmacy | 
-    puts pharmacy ["number"]
-    puts pharmacy ["basic"]["organization_name"]
-end 
+CSV.open("myfile.csv", "w") do |csv|
+    csv << ["Pharmacy Name", "NPI", "City", "State"]
+    parsed_json["results"].each do | pharmacy | 
+        puts pharmacy ["number"]
+        puts pharmacy ["basic"]["organization_name"]
+        csv << [pharmacy ["basic"]["organization_name"], pharmacy ["number"]]
+        binding.pry
+    end 
+
+  
+  # ...
+end
