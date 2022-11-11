cd794274c8b31a17f7b6cb7e2e090006aa536bf9
Adam Harbert <Adam.harbert@gmail.com>
2022-11-11 09:27:39 -0500

    Updated URL search parameters

--- pharmacylookup.rb
+++ pharmacylookup.rb
@@ -1,6 +1,6 @@
 require 'faraday'
 require 'pry'
-response = Faraday.get('https://npiregistry.cms.hhs.gov/api/?number=&enumeration_type=&taxonomy_description=&first_name=&use_first_name_alias=&last_name=&organization_name=Onco360&address_purpose=&city=&state=&postal_code=&country_code=&limit=&skip=&pretty=&version=2.1')
+response = Faraday.get('https://npiregistry.cms.hhs.gov/api/?organization_name=Onco360&city=&state=&version=2.1')
 #puts response.body
 parsed_json = JSON.parse(response.body)
 parsed_json["results"].each do | pharmacy | 
